16bf4319478cdafa80738c76dfbb409dd79d5890
claudiogocella <104107475+claudiogocella@users.noreply.github.com>
2022-05-18 17:57:33 -0300

    Initial commit

---
+++ src/ejercicio1.ts
@@ -0,0 +1,11 @@
+console.log("Crear un posteo en Facebook")
+
+console.log("loguearme en Facebook")
+console.log("Clickear la opciòn compartir imagen")
+console.log("Seleccionar una foto de la carpeta imagenes")
+console.log("subir al muro de Facebook")
+console.log("Escribir un comentario referido a la imagen")
+console.log("Presionar el botón publicar")
+
+
+
